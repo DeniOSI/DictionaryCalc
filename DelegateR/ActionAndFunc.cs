57ecb2cab8b0e3ca4d63c336b3b9056285608bd5
DeniOSI <geksabyte@gmail.com>
2016-11-24 22:47:00 +0200

    first

---
+++ DelegateR/ActionAndFunc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DelegateR
+{
+    
+    class ActionAndFunc
+    {
+        #region Anonymous Function
+        //private delegate double OperationChoise(double x, double y);
+        //private Dictionary<string, OperationChoise> _op;
+        //public ActionAndFunc()
+        //{
+        //    _op = new Dictionary<string, OperationChoise>
+        //    {
+        //        {  "+", delegate (double x, double y)  {return x+y; } }
+        //        , {"-", delegate(double x, double y) {return x-y; } },
+        //        { "*", delegate(double x, double y) {return x*y; } }, 
+        //        { "/", delegate(double x, double y) {return x/y; } }
+        //    };
+
+        //} 
+        #endregion
+
+        #region Lymbda
+
+        
+        private delegate double OperationChoise(double x, double y);
+        private Dictionary<string, Func<double, double, double>> _op;
+        public ActionAndFunc()
+        {
+            _op = new Dictionary<string, Func<double, double, double>>
+            {
+                {  "+", (x, y) => x+y },
+                { "-", (x, y) => x-y },
+                { "*", delegate(double x, double y) {return x*y; } },
+                { "/", delegate(double x, double y) {return x/y; } }
+            };
+
+        }
+        #endregion
+
+        public double PerfOperation(string op, double x, double y)
+        {
+            if (!_op.ContainsKey(op)) throw new ArgumentException(string.Format("operation {0} is invalid {1}", op), "op");
+            return _op [op](x, y) ;
+        }
+        public void AddOperation(string oper, Func<double, double, double> Function)
+        {
+            if(_op.ContainsKey(oper)) return;
+            _op.Add(oper, Function);
+        }
+       
+    }
+}
---
+++ DelegateR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActionAndFunc aaf = new ActionAndFunc();
+            aaf.AddOperation("mod", (x, y) => x % y);
+            char choise = 'o';
+            do
+            {
+                Console.WriteLine("Enter operator and operation. \n Enter first operand");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operator + or - or * or /");
+                string operand = Console.ReadLine();
+                Console.WriteLine("Enter second operand");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine(aaf.PerfOperation(operand, x, y));
+                Console.WriteLine();
+                Console.WriteLine("If you want finish enter \"X\"");
+                choise = char.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            while (choise != 'X');
+            
+        }
+    }
+}
